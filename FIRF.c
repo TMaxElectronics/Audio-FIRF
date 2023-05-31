b85a7d9adff6ca35331aff2b61c5bf65d9056843
Thorben Zethoff <Thorben.Zethoff@tmax-electronics.de>
2023-05-31 14:32:43 +0200

    started adding multi firf

--- FIRF.c
+++ FIRF.c
@@ -4,15 +4,32 @@
 #include "message_buffer.h"
 #include "task.h"
 #include "FIRF.h"
+#include "DLL.h"
 
-static AudioSample_t FIRF_filter(FIRF_t * filter, AudioSample_t inputSample){
+static DLLObject * sampleSources;
+static DLLObject * FIRFs;
+
+#define FIRF_SCALE_ITEMS(FIRF, SCALE) (SCALE * FIRF->filterLength) >> 16
+
+//force Os optimisation for this part of the code. Necessary to get decent performance
+#pragma GCC push_options
+#pragma GCC optimize ("Os")
+static AudioSample_t FIRF_filter(FIRF_t * filter, AudioSample_t inputSample, uint32_t itemsToCalculate){
+    //limit size variable
+    if(itemsToCalculate > filter->filterLength) itemsToCalculate = filter->filterLength;
+    
     int32_t accumulator = 0;
     filter->samples[filter->currFilterStartIndex] = inputSample;
     
+    //calculate pointers
     int32_t * sample = &(filter->samples[filter->currFilterStartIndex]);
     int32_t * coefficient = filter->coefficients;
     int32_t * INVALID_firstInvalidPointer = &(filter->samples[filter->filterLength]);
-    int32_t * INVALID_endPointer = sample; //end once we have reached the start again
+    
+    //end pointer is the first pointer that must not be calculated. If itemsToCalculate is filterLength then it must be the pointer to the sample that was just added
+    uint32_t lastID = filter->currFilterStartIndex + itemsToCalculate;
+    if(lastID >= filter->filterLength) lastID -= filter->filterLength;
+    int32_t * INVALID_endPointer = &(filter->samples[lastID]); //end once we have reached the start again
     
     do{
         accumulator += (*(sample++) * *(coefficient++)) >> 16;
@@ -23,7 +40,32 @@ static AudioSample_t FIRF_filter(FIRF_t * filter, AudioSample_t inputSample){
     
     return accumulator;
 }
+#pragma GCC pop_options
 
+//tasks for all FIRF calculations
+static void FIRF_task(void * taskData){
+    uint32_t speedScaler = FIRF_SPEED_NORMAL;
+    
+    while(1){
+        //go through the list of sample sources, get a sample and check if anybody needs to calculate it. This does mean samples with no data sink are discarded
+        DLL_FOREACH(currSrc, sampleSources){
+            //get the sample data
+            FIRF_DataSource_t * src = (FIRF_DataSource_t *) (currSrc->data);
+            AudioSample_t currSample = (*(src->sampleRequestCallback))(src->callbackData);
+            
+            //check if a FIRF needs this data
+            DLL_FOREACH(currFRIF, FIRFs){
+                FIRF_t * firf = (FIRF_t *) (currFRIF->data);
+                if(firf->active && firf->dataSourceID == src.ID){
+                    //yes, run the calculation with the amount of items to calculate scaled by the overall speed scale
+                    FIRF_filter(firf, currSample, FIRF_SCALE_ITEMS(firf, speedScaler));
+                }
+            }
+        }
+    }
+}
+
+/*
 static void FIRF_task(void * taskData){
     FIRF_t * filter = (FIRF_t *) taskData;
     
@@ -37,7 +79,7 @@ static void FIRF_task(void * taskData){
     }
     
     //TODO free data
-}
+}*/
 
 FIRF_t * FIRF_create(uint32_t filterLength){
     FIRF_t * ret = pvPortMalloc(sizeof(FIRF_t));
@@ -54,7 +96,7 @@ FIRF_t * FIRF_create(uint32_t filterLength){
     
     ret->coefficients[0] = 0xffff;
     
-    xTaskCreate(FIRF_task, "Audio Task", configMINIMAL_STACK_SIZE, (void*) ret, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(FIRF_task, "FIRF Task", configMINIMAL_STACK_SIZE, (void*) ret, tskIDLE_PRIORITY + 1, NULL);
     
     return ret;
 }
--- include/FIRF.h
+++ include/FIRF.h
@@ -5,10 +5,24 @@
 #include "message_buffer.h"
 #include "task.h"
 
+typedef struct _FIRF_t_ FIRF_t;
+typedef struct _FIRF_DataSource_t_ FIRF_DataSource_t;
 typedef int16_t AudioSample_t;
 
-typedef struct{
-    StreamBufferHandle_t inputBuffer;
+#define FIRF_SPEED_NORMAL 0x10000
+
+typedef int32_t (* FIRF_sampleRequestCallback)(void * data);
+
+struct _FIRF_DataSource_t_{
+    FIRF_sampleRequestCallback sampleRequestCallback;
+    void * callbackData;
+    
+    uint32_t ID;
+};
+
+struct _FIRF_t_{
+    uint32_t dataSourceID;
+    
     StreamBufferHandle_t outputBuffer;
     
     int32_t * coefficients;
@@ -18,7 +32,7 @@ typedef struct{
     uint32_t filterLength;
     
     uint32_t active;
-} FIRF_t;
+};
 
 FIRF_t * FIRF_create(uint32_t filterLength);
 
