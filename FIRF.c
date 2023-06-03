222a72d6d2d9e412a71838073aafddd32a32be4e
TMaxElectronics <60486146+TMaxElectronics@users.noreply.github.com>
2023-06-04 01:10:28 +0200

    implemented multi source & sink firf

--- FIRF.c
+++ FIRF.c
@@ -2,12 +2,16 @@
 
 #include "FreeRTOS.h"
 #include "message_buffer.h"
+#include "semphr.h"
 #include "task.h"
 #include "FIRF.h"
 #include "DLL.h"
 
 static DLLObject * sampleSources;
 static DLLObject * FIRFs;
+static SemaphoreHandle_t firfListsSemaphore;
+
+#define FIRF_batchSize 48
 
 #define FIRF_SCALE_ITEMS(FIRF, SCALE) (SCALE * FIRF->filterLength) >> 16
 
@@ -40,30 +44,53 @@ static AudioSample_t FIRF_filter(FIRF_t * filter, AudioSample_t inputSample, uin
     
     return accumulator;
 }
-#pragma GCC pop_options
 
 //tasks for all FIRF calculations
 static void FIRF_task(void * taskData){
     uint32_t speedScaler = FIRF_SPEED_NORMAL;
     
+    AudioSample_t * sampleBuffer = pvPortMalloc(sizeof(AudioSample_t) * FIRF_batchSize);
+    
     while(1){
-        //go through the list of sample sources, get a sample and check if anybody needs to calculate it. This does mean samples with no data sink are discarded
-        DLL_FOREACH(currSrc, sampleSources){
-            //get the sample data
-            FIRF_DataSource_t * src = (FIRF_DataSource_t *) (currSrc->data);
-            AudioSample_t currSample = (*(src->sampleRequestCallback))(src->callbackData);
-            
-            //check if a FIRF needs this data
-            DLL_FOREACH(currFRIF, FIRFs){
-                FIRF_t * firf = (FIRF_t *) (currFRIF->data);
-                if(firf->active && firf->dataSourceID == src.ID){
-                    //yes, run the calculation with the amount of items to calculate scaled by the overall speed scale
-                    FIRF_filter(firf, currSample, FIRF_SCALE_ITEMS(firf, speedScaler));
+        //TODO evaluate of this ever returns to other code... priorities and stuff
+        //if(xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)){
+            //go through the list of sample sources, get a sample and check if anybody needs to calculate it. This does mean samples with no data sink are discarded
+            DLL_FOREACH(currSrc, sampleSources){
+                                //LATBSET = _LATB_LATB2_MASK;
+                //get the sample data, as much as possible up to the maximum batch size
+                FIRF_DataSource_t * src = (FIRF_DataSource_t *) (currSrc->data);
+                
+                uint32_t samplesReceived = (*(src->sampleRequestCallback))(src->callbackData, sampleBuffer, FIRF_batchSize);
+
+
+                //did we actually read any data?
+                if(samplesReceived > 0){
+                    //check if a FIRF needs this data
+                    DLL_FOREACH(currFRIF, FIRFs){
+                        FIRF_t * firf = (FIRF_t *) (currFRIF->data);
+                        if(firf->active && firf->dataSource == src){
+                            
+                            //yes, run the calculation for each of the samples with the amount of items to calculate scaled by the overall speed scale
+                            for(uint32_t currSample = 0; currSample < samplesReceived; currSample++){
+                                sampleBuffer[currSample] = FIRF_filter(firf, sampleBuffer[currSample], FIRF_SCALE_ITEMS(firf, speedScaler));
+                            }
+                            
+                            //and finally write the data to all sinks
+                            DLL_FOREACH(currSink, firf->dataSinks){
+                                FIRF_DataSink_t * sink = (FIRF_DataSink_t *) currSink->data;
+                                (*(sink->sampleReturnCallback))(sink->callbackData, sampleBuffer, samplesReceived);
+                            }
+                        }
+                    }
                 }
+                                //LATBCLR = _LATB_LATB2_MASK;
             }
-        }
+        //}
+        //xSemaphoreGive(firfListsSemaphore);
+            vTaskDelay(1);
     }
 }
+#pragma GCC pop_options
 
 /*
 static void FIRF_task(void * taskData){
@@ -81,11 +108,125 @@ static void FIRF_task(void * taskData){
     //TODO free data
 }*/
 
+FIRF_DataSink_t * FIRF_addDataSink(FIRF_t * filter, FIRF_sampleReturnCallback callback, void* callbackData){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return NULL;
+            
+    FIRF_DataSink_t * sink = pvPortMalloc(sizeof(FIRF_DataSink_t));
+    
+    sink->sampleReturnCallback = callback;
+    sink->callbackData = callbackData;
+    
+    DLL_add(sink, filter->dataSinks);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
+}
+
+void FIRF_removeSignalSink(FIRF_t * filter, FIRF_sampleReturnCallback * sink){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return NULL;
+    
+    //remove the sink from the DLL
+    DLL_removeData(sink, filter->dataSinks);
+    
+    vPortFree(sink);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
+}
+
+FIRF_DataSource_t * FIRF_registerSignalSource(FIRF_sampleRequestCallback callback, void* callbackData){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return NULL;
+    
+    //initialise source struct
+    FIRF_DataSource_t * ret = pvPortMalloc(sizeof(FIRF_DataSource_t));
+    
+    ret->sampleRequestCallback = callback;
+    ret->callbackData = callbackData;
+    
+    //add the new source to the DLL
+    DLL_add(ret, sampleSources);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
+    
+    return ret;
+}
+
+void FIRF_removeSignalSource(FIRF_DataSource_t * source){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return;
+    
+    //remove all pointers to this source from the firf list (set them to NULL)
+    DLL_FOREACH(currFIRF, FIRFs){
+        FIRF_t * firf = (FIRF_t *) currFIRF->data;
+        if(firf->dataSource == source) firf->dataSource = NULL;
+    }
+    
+    //now remove the source from the source DLL
+    DLL_removeData(source, sampleSources);
+    
+    //and finally free the struct
+    vPortFree(source);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
+}
+
+void FIRF_setSignalSource(FIRF_t * filter, FIRF_DataSource_t * source){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return;
+    
+    uint32_t found = 0;
+    //first check if the source is even registered
+    DLL_FOREACH(currSrc, sampleSources){
+        FIRF_DataSource_t * src = (FIRF_DataSource_t *) currSrc->data;
+        
+        if(src == source){ 
+            found = 1;
+            break;
+        }
+    }
+    
+    if(!found){
+        //hmm source is not registered. Assigning it to a firf would cause weirdness, so just return (and remember to return the semaphore)
+        xSemaphoreGive(firfListsSemaphore);
+        return;
+    }
+    
+    //ok signal source is registered, all we need to do is to assign the pointer in the firf struct
+    filter->dataSource = source;
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
+}
+
+void FIRF_init(){
+    //create lists
+    sampleSources = DLL_create();
+    FIRFs = DLL_create();
+    
+    //create semaphore and make sure its available
+    firfListsSemaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(firfListsSemaphore);
+    
+    //create the task, lowest system wide priority
+    xTaskCreate(FIRF_task, "FIRF Task", configMINIMAL_STACK_SIZE+128, NULL, tskIDLE_PRIORITY + 1, NULL);
+}
+
+void FIRF_setTransferFunction(FIRF_t * filter){
+    //TODO!
+}
+
 FIRF_t * FIRF_create(uint32_t filterLength){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return NULL;
+    
     FIRF_t * ret = pvPortMalloc(sizeof(FIRF_t));
     
-    ret->inputBuffer = xStreamBufferCreate(sizeof(AudioSample_t) * filterLength, 0);
-    ret->outputBuffer = xStreamBufferCreate(sizeof(AudioSample_t) * filterLength, 0);
+    ret->dataSinks = DLL_create();
     
     ret->samples = pvPortMalloc(sizeof(int32_t) * filterLength);
     ret->coefficients = pvPortMalloc(sizeof(int32_t) * filterLength);
@@ -94,9 +235,40 @@ FIRF_t * FIRF_create(uint32_t filterLength){
     ret->currFilterStartIndex = 0;
     ret->filterLength = filterLength;
     
-    ret->coefficients[0] = 0xffff;
+    //set a unity gain transfer function
+    memset(ret->coefficients, 0, sizeof(int32_t) * filterLength);
+    ret->coefficients[0] = FIRF_UNITYGAIN;
     
-    xTaskCreate(FIRF_task, "FIRF Task", configMINIMAL_STACK_SIZE, (void*) ret, tskIDLE_PRIORITY + 1, NULL);
+    //add the filter to the list
+    DLL_add(ret, FIRFs);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
     
     return ret;
+}
+
+void FIRF_destroy(FIRF_t * filter){
+    //get the list semaphore, so we can be sure the FOREACH won't do stupid stuff
+    if(!xSemaphoreTake(firfListsSemaphore, portMAX_DELAY)) return NULL;
+    
+    //remove the filter from the list if its in it
+    DLL_removeData(filter, FIRFs);
+    
+    //remove all sinks and free the DLL
+    DLL_FOREACH(currSink, filter->dataSinks){
+        FIRF_DataSink_t * sink = (FIRF_DataSink_t *) currSink->data;
+        DLL_remove(currSink);
+        vPortFree(sink);
+    }
+    DLL_free(filter->dataSinks);
+    
+    //free the FIRF
+    vPortFree(filter->samples);
+    vPortFree(filter->coefficients);
+    
+    vPortFree(filter);
+    
+    //return the semaphore
+    xSemaphoreGive(firfListsSemaphore);
 }
--- include/FIRF.h
+++ include/FIRF.h
@@ -4,26 +4,35 @@
 #include "FreeRTOS.h"
 #include "message_buffer.h"
 #include "task.h"
+#include "DLL.h"
 
 typedef struct _FIRF_t_ FIRF_t;
 typedef struct _FIRF_DataSource_t_ FIRF_DataSource_t;
+typedef struct _FIRF_DataSink_t_ FIRF_DataSink_t;
 typedef int16_t AudioSample_t;
 
 #define FIRF_SPEED_NORMAL 0x10000
+#define FIRF_UNITYGAIN 0x10000
 
-typedef int32_t (* FIRF_sampleRequestCallback)(void * data);
+
+typedef uint32_t (* FIRF_sampleRequestCallback)(void * usrData, AudioSample_t * dst, uint32_t dstSize);
+typedef void (* FIRF_sampleReturnCallback)(void * usrData, AudioSample_t * src, uint32_t dstSize);
 
 struct _FIRF_DataSource_t_{
     FIRF_sampleRequestCallback sampleRequestCallback;
     void * callbackData;
-    
-    uint32_t ID;
+};
+
+struct _FIRF_DataSink_t_{
+    FIRF_sampleReturnCallback sampleReturnCallback;
+    void * callbackData;
 };
 
 struct _FIRF_t_{
-    uint32_t dataSourceID;
+    //WARNING: keep in mind that if the datasource is removed while a FIRF is still pointing to it this pointer will become invalid
+    FIRF_DataSource_t * dataSource;
     
-    StreamBufferHandle_t outputBuffer;
+    DLLObject * dataSinks;
     
     int32_t * coefficients;
     int32_t * samples;
@@ -34,6 +43,14 @@ struct _FIRF_t_{
     uint32_t active;
 };
 
+void FIRF_init();
+void FIRF_setTransferFunction(FIRF_t * filter);
 FIRF_t * FIRF_create(uint32_t filterLength);
+void FIRF_destroy(FIRF_t * filter);
+void FIRF_setSignalSource(FIRF_t * filter, FIRF_DataSource_t * source);
+void FIRF_removeSignalSource(FIRF_DataSource_t * source);
+FIRF_DataSource_t * FIRF_registerSignalSource(FIRF_sampleRequestCallback callback, void* callbackData);
+void FIRF_removeSignalSink(FIRF_t * filter, FIRF_sampleReturnCallback * sink);
+FIRF_DataSink_t * FIRF_addDataSink(FIRF_t * filter, FIRF_sampleReturnCallback callback, void* callbackData);
 
 #endif
